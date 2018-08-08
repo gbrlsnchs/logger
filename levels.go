5efe645953381de21199e1769e161304cb9abb8b
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-08 14:53:42 -0300

    Remove Level type alias for better interfacing with custom uint8 args

--- levels.go
+++ levels.go
@@ -1,20 +1,25 @@
 package logger
 
-// Level is a log level.
 // LevelOff alone turns all logs off.
+//
 // LevelAll turns all logs on whenever present in the
-// log level caculation. Log levels' bits are or'ed together,
+// log level caculation.
+//
+// Log levels' bits are or'ed together,
 // so, regardless of the order they appear in the caculation,
 // if they're present in it, they're turned on.
+//
 // For example, to turn only Fatal e Debug levels on,
 // the level should be set the following way:
 // 	l := logger.New(logger.LevelFatal | logger.LevelDebug, os.Stdout, os.Stderr)
-type Level uint8
-
+// To turn all levels off:
+// 	l := logger.New(logger.LevelOff, os.Stdout, os.Stderr)
+// And to turn all on:
+// 	l := logger.New(logger.LevelAll, os.Stdout, os.Stderr)
 const (
-	LevelOff   Level = 0
-	LevelAll   Level = 255
-	LevelFatal Level = 1 << iota
+	LevelOff   uint8 = 0
+	LevelAll   uint8 = 255
+	LevelFatal uint8 = 1 << iota
 	LevelError
 	LevelWarn
 	LevelInfo
